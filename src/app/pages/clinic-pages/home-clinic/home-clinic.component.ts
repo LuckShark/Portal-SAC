711fa454cdb38b309c8333eacfbf7962c9aa206d
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-10-14 07:51:33 -0300

    header,footer e hero da clínica funcionais, prontos para colocar layout

--- src/app/pages/clinic-pages/home-clinic/home-clinic.component.ts
+++ src/app/pages/clinic-pages/home-clinic/home-clinic.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderSaudeComponent } from "../../../components/header-saude/header-saude.component";
+import { HeroHomeClinicComponent } from "../hero-home-clinic/hero-home-clinic.component";
+import { FooterSaudeComponent } from "../../../components/footer-saude/footer-saude.component";
 
 @Component({
   selector: 'app-home-clinic',
   standalone: true,
-  imports: [],
+  imports: [HeaderSaudeComponent, HeroHomeClinicComponent, FooterSaudeComponent],
   templateUrl: './home-clinic.component.html',
   styleUrl: './home-clinic.component.scss'
 })
