1eed56304299934f423d03b1160a8a59c1b6bdc1
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-10-21 08:44:10 -0300

    Hero Clinic nos moldes

--- src/app/pages/clinic-pages/hero-home-clinic/hero-home-clinic.component.ts
+++ src/app/pages/clinic-pages/hero-home-clinic/hero-home-clinic.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CarrosselComponent } from "../../../shared/carrossel/carrossel.component";
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 @Component({
   selector: 'app-hero-home-clinic',
   standalone: true,
-  imports: [],
+  imports: [CarrosselComponent, CommonModule, NgOptimizedImage, MatButtonModule, MatTabsModule],
   templateUrl: './hero-home-clinic.component.html',
   styleUrl: './hero-home-clinic.component.scss'
 })
