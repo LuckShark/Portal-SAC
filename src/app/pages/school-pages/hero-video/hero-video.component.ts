87656ee888ac12160165508e7a498f0e4f922d4a
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-10-24 15:39:08 -0300

    add video + stuffs

---
+++ src/app/pages/school-pages/hero-video/hero-video.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-video',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-video.component.html',
+  styleUrl: './hero-video.component.scss'
+})
+export class HeroVideoComponent {
+
+}
--- src/app/pages/school-pages/home-social/home-social.component.ts
+++ src/app/pages/school-pages/home-social/home-social.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderSocialComponent } from "../../../components/header-social/header-social.component";
 import { HeroSocialComponent } from "../hero-social/hero-social.component";
 import { FooterSocialComponent } from "../../../components/footer-social/footer-social.component";
+import { HeroVideoComponent } from "../hero-video/hero-video.component";
 
 @Component({
   selector: 'app-home-social',
   standalone: true,
-  imports: [HeaderSocialComponent, HeroSocialComponent, FooterSocialComponent],
+  imports: [HeaderSocialComponent, HeroSocialComponent, FooterSocialComponent, HeroVideoComponent],
   templateUrl: './home-social.component.html',
   styleUrl: './home-social.component.scss'
 })
