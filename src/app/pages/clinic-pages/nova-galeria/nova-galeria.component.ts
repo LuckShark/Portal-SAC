8f3a75d1beb3122fbd588aa1107503168cd87545
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-07 14:24:02 -0300

    gallery ok I think

--- src/app/pages/clinic-pages/nova-galeria/nova-galeria.component.ts
+++ src/app/pages/clinic-pages/nova-galeria/nova-galeria.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { NavegacaoService } from '../../../services/navegacao.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nova-galeria',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './nova-galeria.component.html',
   styleUrl: './nova-galeria.component.scss'
 })
 export class NovaGaleriaComponent {
+
+  constructor(private navegacaoService: NavegacaoService) {}
+
+  navegar(rota: string) {
+    this.navegacaoService.navegarParaRota(rota);
+  }
 
 }
