728c96e37c3c9e8ced07ea40d7acb5b0da4afc98
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-22 11:08:25 -0300

    correção do header

--- src/app/components/header-saude/header-saude.component.ts
+++ src/app/components/header-saude/header-saude.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavegacaoService } from '../../services/navegacao.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header-saude',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header-saude.component.html',
   styleUrl: './header-saude.component.scss'
 })
 export class HeaderSaudeComponent {
+
+  isMenuOpen = false;
 
   constructor(private navegacaoService: NavegacaoService) {}
 
