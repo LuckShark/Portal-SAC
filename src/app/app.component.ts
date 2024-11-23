c0c9175c046318052b33463c198a3989651f0285
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-22 21:39:27 -0300

    pop up funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/first-page/home/home.component";
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portal-sac';
+export class AppComponent implements OnInit{
+  showPopup = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    // Verifica se está no navegador
+    if (isPlatformBrowser(this.platformId)) {
+      const popupShown = localStorage.getItem('popupShown');
+      if (!popupShown) {
+        this.showPopup = true;
+        localStorage.setItem('popupShown', 'true');
+      }
+    }
+  }
+
+  closePopup() {
+    this.showPopup = false;
+  }
 }
