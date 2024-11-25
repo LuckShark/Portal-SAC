505d1965cce67a45c3bd5e715df29a6b7423a1dd
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-25 19:29:43 -0300

    fix finais do dia

--- src/app/pages/clinic-pages/home-cirurgias/galeria-cirurgias/galeria-cirurgias.component.ts
+++ src/app/pages/clinic-pages/home-cirurgias/galeria-cirurgias/galeria-cirurgias.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavegacaoService } from '../../../../services/navegacao.service';
 
 @Component({
   selector: 'app-galeria-cirurgias',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './galeria-cirurgias.component.html',
   styleUrl: './galeria-cirurgias.component.scss'
 })
 export class GaleriaCirurgiasComponent {
+
+  constructor(private navegacaoService: NavegacaoService) {}
+
+  navegar(rota: string) {
+    this.navegacaoService.navegarParaRota(rota);
+  }
 
 }
