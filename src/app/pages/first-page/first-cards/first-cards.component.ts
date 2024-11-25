93ad2d682d962de306650c78fee42655d6f5fa85
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-25 15:54:24 -0300

    alguns fixes no header

--- src/app/pages/first-page/first-cards/first-cards.component.ts
+++ src/app/pages/first-page/first-cards/first-cards.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { NavegacaoService } from '../../../services/navegacao.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-first-cards',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './first-cards.component.html',
   styleUrl: './first-cards.component.scss'
 })
 export class FirstCardsComponent {
+
+  constructor(private navegacaoService: NavegacaoService) {}
+
+  navegar(rota: string) {
+    this.navegacaoService.navegarParaRota(rota);
+  }
 
 }
