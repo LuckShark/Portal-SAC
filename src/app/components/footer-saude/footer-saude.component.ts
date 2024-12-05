fd68e856cb89c97e7ebc20bfc3213f6e22d0362b
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-05 11:31:54 -0300

    Footer saude OK - OVERFLOW OK

--- src/app/components/footer-saude/footer-saude.component.ts
+++ src/app/components/footer-saude/footer-saude.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavegacaoService } from '../../services/navegacao.service';
 
 @Component({
   selector: 'app-footer-saude',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './footer-saude.component.html',
   styleUrl: './footer-saude.component.scss'
 })
 export class FooterSaudeComponent {
+
+  constructor(private navegacaoService: NavegacaoService) {}
+
+  navegar(rota: string) {
+    this.navegacaoService.navegarParaRota(rota);
+  }
 
 }
