a7fcb6b7a3b32687d1f6a62ee0762ef2257d7706
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-06 09:54:11 -0300

    consultas OK

--- src/app/pages/clinic-pages/home-consultas/home-consultas.component.ts
+++ src/app/pages/clinic-pages/home-consultas/home-consultas.component.ts
@@ -3,11 +3,12 @@ import { HeaderSaudeComponent } from "../../../components/header-saude/header-sa
 import { HeadingConsultasComponent } from "./heading-consultas/heading-consultas.component";
 import { FooterSaudeComponent } from "../../../components/footer-saude/footer-saude.component";
 import { ConteudoConsultasComponent } from "./conteudo-consultas/conteudo-consultas.component";
+import { PartnersComponent } from "../partners/partners.component";
 
 @Component({
   selector: 'app-home-consultas',
   standalone: true,
-  imports: [HeaderSaudeComponent, HeadingConsultasComponent, FooterSaudeComponent, ConteudoConsultasComponent],
+  imports: [HeaderSaudeComponent, HeadingConsultasComponent, FooterSaudeComponent, ConteudoConsultasComponent, PartnersComponent],
   templateUrl: './home-consultas.component.html',
   styleUrl: './home-consultas.component.scss'
 })
