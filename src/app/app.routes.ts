36b48b9639e219f0c2a5f9219ddb2d2637a4aed1
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-06 11:04:00 -0300

    Cirurgias gallery e redirecionamento funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,17 @@ import { HomeCirurgiasComponent } from './pages/clinic-pages/home-cirurgias/home
 import { ProjetosComponent } from './pages/school-pages/projetos/projetos.component';
 import { Cirurgia1ImplanteAnelComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia1-implante-anel/cirurgia1-implante-anel.component';
 import { Cirurgia2RefrativaComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia2-refrativa/cirurgia2-refrativa.component';
+import { Cirurgia3PlasticaOcularComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia3-plastica-ocular/cirurgia3-plastica-ocular.component';
+import { Cirurgia4CatarataComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia4-catarata/cirurgia4-catarata.component';
+import { Cirurgia5EstrabismoComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia5-estrabismo/cirurgia5-estrabismo.component';
+import { Cirurgia6GlaucomaComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia6-glaucoma/cirurgia6-glaucoma.component';
+import { Cirurgia7RetinaComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia7-retina/cirurgia7-retina.component';
+import { Cirurgia8TransplanteCorneaComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia8-transplante-cornea/cirurgia8-transplante-cornea.component';
+import { Cirurgia9InjecaoComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia9-injecao/cirurgia9-injecao.component';
+import { Cirurgia10BlefaroplastiaComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia10-blefaroplastia/cirurgia10-blefaroplastia.component';
+import { Cirurgia11PterigioComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia11-pterigio/cirurgia11-pterigio.component';
+import { Cirurgia12CalazioComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia12-calazio/cirurgia12-calazio.component';
+import { Cirurgia13TumorComponent } from './pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia13-tumor/cirurgia13-tumor.component';
 
 export const routes: Routes = [
     {
@@ -62,6 +73,50 @@ export const routes: Routes = [
     {
         path: 'cirurgia2-refrativa',
         component: Cirurgia2RefrativaComponent
+    },
+    {
+        path: 'cirurgia3-plastica-ocular',
+        component: Cirurgia3PlasticaOcularComponent
+    },
+    {
+        path: 'cirurgia4-catarata',
+        component: Cirurgia4CatarataComponent
+    },
+    {
+        path: 'cirurgia5-estrabismo',
+        component: Cirurgia5EstrabismoComponent
+    },
+    {
+        path: 'cirurgia6-glaucoma',
+        component: Cirurgia6GlaucomaComponent
+    },
+    {
+        path: 'cirurgia7-retina',
+        component: Cirurgia7RetinaComponent
+    },
+    {
+        path: 'cirurgia8-transplante-cornea',
+        component: Cirurgia8TransplanteCorneaComponent
+    },
+    {
+        path: 'cirurgia9-injecao',
+        component: Cirurgia9InjecaoComponent
+    },
+    {
+        path: 'cirurgia10-blefaroplastia',
+        component: Cirurgia10BlefaroplastiaComponent
+    },
+    {
+        path: 'cirurgia11-pterigio',
+        component: Cirurgia11PterigioComponent
+    },
+    {
+        path: 'cirurgia12-calazio',
+        component: Cirurgia12CalazioComponent
+    },
+    {
+        path: 'cirurgia13-tumor',
+        component: Cirurgia13TumorComponent
     },
     { path: '**', component: HomeComponent },
 ];
