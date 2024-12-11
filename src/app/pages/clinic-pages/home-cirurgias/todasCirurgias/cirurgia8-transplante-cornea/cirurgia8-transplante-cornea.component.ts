6ca9f68557973c4f77d78e3c67e7552c6ead21a9
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-11 17:36:16 -0300

    atençao a mudanças nas cores

--- src/app/pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia8-transplante-cornea/cirurgia8-transplante-cornea.component.ts
+++ src/app/pages/clinic-pages/home-cirurgias/todasCirurgias/cirurgia8-transplante-cornea/cirurgia8-transplante-cornea.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderSaudeComponent } from "../../../../../components/header-saude/header-saude.component";
+import { HeadingCirurgiasComponent } from "../../heading-cirurgias/heading-cirurgias.component";
+import { MarqueSuaConsultaComponent } from "../../../marque-sua-consulta/marque-sua-consulta.component";
+import { FooterSaudeComponent } from "../../../../../components/footer-saude/footer-saude.component";
 
 @Component({
   selector: 'app-cirurgia8-transplante-cornea',
   standalone: true,
-  imports: [],
+  imports: [HeaderSaudeComponent, HeadingCirurgiasComponent, MarqueSuaConsultaComponent, FooterSaudeComponent],
   templateUrl: './cirurgia8-transplante-cornea.component.html',
   styleUrl: './cirurgia8-transplante-cornea.component.scss'
 })
