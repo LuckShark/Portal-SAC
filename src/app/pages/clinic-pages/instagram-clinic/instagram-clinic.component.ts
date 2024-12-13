33065f4a545e780556ad695b0f2fed148b0adfca
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-13 09:04:52 -0300

    galeria exames OK mod em cirurgias

--- src/app/pages/clinic-pages/instagram-clinic/instagram-clinic.component.ts
+++ src/app/pages/clinic-pages/instagram-clinic/instagram-clinic.component.ts
@@ -10,15 +10,6 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrls: ['./instagram-clinic.component.scss']
 })
 export class InstagramClinicComponent {
-  // posts = [
-  //   { image: 'assets/img/clinic/ig-clinic/ig1.png', caption: 'A íris de cada pessoa', link: 'https://www.instagram.com/p/DBgmzOQxjiO/' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig2.png', caption: 'Atletas do Instituto', link: 'https://www.instagram.com/p/DBeB6-YR7BL/' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig3.png', caption: 'F1 Nas escolas', link: 'https://www.instagram.com/p/DBbdDynxXyf/' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig4.png', caption: '82 anos da SAC', link: 'https://www.instagram.com/p/DAn-LQ8O2zG/?img_index=1' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig5.png', caption: 'Função Macular', link: 'https://www.instagram.com/p/DAJFpMtutli/' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig6.png', caption: 'Dispositivos de VR', link: 'https://www.instagram.com/p/C_lJq-bu0h7/' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig7.png', caption: 'Catarata e cegueira', link: 'https://www.instagram.com/p/C_atdR2ujMR/' },
-  //   { image: 'assets/img/clinic/ig-clinic/ig8.png', caption: 'Catarata e cegueira', link: 'https://www.instagram.com/p/C_akDWxuxgU/' },
-  // ];
+  
 
 }
