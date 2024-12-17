2bcd63bee73cd2151a83e04c8daba5e729dade1b
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-17 10:35:55 -0300

    até exame 13

--- src/app/pages/clinic-pages/home-exames/todosExames/exame12-iridectomia/exame12-iridectomia.component.ts
+++ src/app/pages/clinic-pages/home-exames/todosExames/exame12-iridectomia/exame12-iridectomia.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterSaudeComponent } from "../../../../../components/footer-saude/footer-saude.component";
+import { MarqueSuaConsultaComponent } from "../../../marque-sua-consulta/marque-sua-consulta.component";
+import { HeadingExamesComponent } from "../../heading-exames/heading-exames.component";
+import { HeaderSaudeComponent } from "../../../../../components/header-saude/header-saude.component";
 
 @Component({
   selector: 'app-exame12-iridectomia',
   standalone: true,
-  imports: [],
+  imports: [FooterSaudeComponent, MarqueSuaConsultaComponent, HeadingExamesComponent, HeaderSaudeComponent],
   templateUrl: './exame12-iridectomia.component.html',
   styleUrl: './exame12-iridectomia.component.scss'
 })
--- src/app/pages/clinic-pages/home-exames/todosExames/exame13-mapeamento/exame13-mapeamento.component.ts
+++ src/app/pages/clinic-pages/home-exames/todosExames/exame13-mapeamento/exame13-mapeamento.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterSaudeComponent } from "../../../../../components/footer-saude/footer-saude.component";
+import { MarqueSuaConsultaComponent } from "../../../marque-sua-consulta/marque-sua-consulta.component";
+import { HeadingExamesComponent } from "../../heading-exames/heading-exames.component";
+import { HeaderSaudeComponent } from "../../../../../components/header-saude/header-saude.component";
 
 @Component({
   selector: 'app-exame13-mapeamento',
   standalone: true,
-  imports: [],
+  imports: [FooterSaudeComponent, MarqueSuaConsultaComponent, HeadingExamesComponent, HeaderSaudeComponent],
   templateUrl: './exame13-mapeamento.component.html',
   styleUrl: './exame13-mapeamento.component.scss'
 })
