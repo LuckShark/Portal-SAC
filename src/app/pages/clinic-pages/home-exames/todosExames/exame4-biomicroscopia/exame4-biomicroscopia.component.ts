b8157fe5c77a3fa13488177bd827d967902ccd54
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-17 08:03:04 -0300

    imagens add / exames quase ok

--- src/app/pages/clinic-pages/home-exames/todosExames/exame4-biomicroscopia/exame4-biomicroscopia.component.ts
+++ src/app/pages/clinic-pages/home-exames/todosExames/exame4-biomicroscopia/exame4-biomicroscopia.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterSaudeComponent } from "../../../../../components/footer-saude/footer-saude.component";
+import { MarqueSuaConsultaComponent } from "../../../marque-sua-consulta/marque-sua-consulta.component";
+import { HeadingExamesComponent } from "../../heading-exames/heading-exames.component";
+import { HeaderSaudeComponent } from "../../../../../components/header-saude/header-saude.component";
 
 @Component({
   selector: 'app-exame4-biomicroscopia',
   standalone: true,
-  imports: [],
+  imports: [FooterSaudeComponent, MarqueSuaConsultaComponent, HeadingExamesComponent, HeaderSaudeComponent],
   templateUrl: './exame4-biomicroscopia.component.html',
   styleUrl: './exame4-biomicroscopia.component.scss'
 })
