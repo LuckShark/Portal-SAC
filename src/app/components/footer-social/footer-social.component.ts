46271af83ea26aca8700c925e1fdf7bb1cadba55
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-19 13:01:42 -0300

    footer social ok

--- src/app/components/footer-social/footer-social.component.ts
+++ src/app/components/footer-social/footer-social.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavegacaoService } from '../../services/navegacao.service';
 
 @Component({
   selector: 'app-footer-social',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './footer-social.component.html',
   styleUrl: './footer-social.component.scss'
 })
 export class FooterSocialComponent {
+
+  constructor(private navegacaoService: NavegacaoService) {}
+  
+    navegar(rota: string) {
+      this.navegacaoService.navegarParaRota(rota);
+    }
 
 }
