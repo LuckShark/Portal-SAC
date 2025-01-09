6bd7e9ff15d23c55ebadb2dfa7f7add08184b8f4
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2025-01-09 09:25:22 -0300

    começo dos cards social

--- src/app/pages/assist-pages/assist-cards/assist-cards.component.ts
+++ src/app/pages/assist-pages/assist-cards/assist-cards.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-assist-cards',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './assist-cards.component.html',
   styleUrl: './assist-cards.component.scss'
 })
