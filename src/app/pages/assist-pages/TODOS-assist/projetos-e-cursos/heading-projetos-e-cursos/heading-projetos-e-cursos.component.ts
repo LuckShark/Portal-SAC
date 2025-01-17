676d26ebf20450c3fb5069ba7f754770a90f8736
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2025-01-17 12:56:38 -0300

    cultura parcialmente ok

--- src/app/pages/assist-pages/TODOS-assist/projetos-e-cursos/heading-projetos-e-cursos/heading-projetos-e-cursos.component.ts
+++ src/app/pages/assist-pages/TODOS-assist/projetos-e-cursos/heading-projetos-e-cursos/heading-projetos-e-cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavegacaoService } from '../../../../../services/navegacao.service';
 
 @Component({
   selector: 'app-heading-projetos-e-cursos',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './heading-projetos-e-cursos.component.scss'
 })
 export class HeadingProjetosECursosComponent {
+
+  constructor(private navegacaoService: NavegacaoService) {}
+      
+        navegar(rota: string) {
+          this.navegacaoService.navegarParaRota(rota);
+        }
 
 }
--- src/app/pages/assist-pages/TODOS-assist/projetos-e-cursos/projetos-e-cursos.component.ts
+++ src/app/pages/assist-pages/TODOS-assist/projetos-e-cursos/projetos-e-cursos.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderAssistComponent } from "../../../../components/header-assist/header-assist.component";
+import { FooterAssistComponent } from "../../../../components/footer-assist/footer-assist.component";
+import { WhatsappSchoolComponent } from "../../../../components/whatsapp-school/whatsapp-school.component";
+import { GalleryFortalecendoComponent } from "../fortalecendo/gallery-fortalecendo/gallery-fortalecendo.component";
+import { HeadingProjetosECursosComponent } from "./heading-projetos-e-cursos/heading-projetos-e-cursos.component";
+import { IntroProjetosECursosComponent } from "./intro-projetos-e-cursos/intro-projetos-e-cursos.component";
+import { ButtonsComponent } from "./buttons/buttons.component";
 
 @Component({
   selector: 'app-projetos-e-cursos',
   standalone: true,
-  imports: [],
+  imports: [HeaderAssistComponent, FooterAssistComponent, WhatsappSchoolComponent, GalleryFortalecendoComponent, HeadingProjetosECursosComponent, IntroProjetosECursosComponent, ButtonsComponent],
   templateUrl: './projetos-e-cursos.component.html',
   styleUrl: './projetos-e-cursos.component.scss'
 })
