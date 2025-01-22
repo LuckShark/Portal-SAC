01716eee96ffc77acd8df9d64434d34e9f831c70
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2025-01-22 09:27:38 -0300

    Todos os headers (+mobile ex) corrigidos e com redirects

--- src/app/components/header-assist/header-assist.component.ts
+++ src/app/components/header-assist/header-assist.component.ts
@@ -1,12 +1,15 @@
+import { DoacaoDialogComponent } from './../header-saude/header-saude.component';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NavegacaoService } from '../../services/navegacao.service';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header-assist',
   standalone: true,
   imports: [
-    CommonModule
+        CommonModule,
+        MatDialogModule
   ],
   templateUrl: './header-assist.component.html',
   styleUrl: './header-assist.component.scss'
@@ -15,10 +18,29 @@ export class HeaderAssistComponent {
 
    isMenuOpen = false;
   
-    constructor(private navegacaoService: NavegacaoService) {}
+    constructor(private navegacaoService: NavegacaoService,  private dialog: MatDialog) {}
   
     navegar(rota: string) {
       this.navegacaoService.navegarParaRota(rota);
     }
 
+    openDoacaoModal() {
+      this.dialog.open(DoacaoDialogComponent, {
+        maxWidth: '90vw',
+        maxHeight: '90vh',
+        data: {
+          qrCodePath: 'assets/img/doacao.png',
+          instructions: 'Por favor, utilize as informações abaixo para realizar sua doação:',
+          detalhesDoacao: {
+            chavePix: '07.018.138/0001-67',
+            agencia: '2937-8',
+            conta: '119980-3',
+            nome: 'Sociedade de Assistência aos Cegos',
+            cnpj: '07.018.138/0001-67'
+          }
+        }
+      });
+    }
+
 }
+
