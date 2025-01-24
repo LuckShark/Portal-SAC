4f6b36a33144fec57740ecd5456be5449acd2c9d
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2025-01-24 11:39:47 -0300

    ESPORTE ok

--- src/app/pages/school-pages/setores/esporte/esporte.component.ts
+++ src/app/pages/school-pages/setores/esporte/esporte.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeadingEsporteComponent } from "./heading-esporte/heading-esporte.component";
 import { HeaderSocialComponent } from "../../../../components/header-social/header-social.component";
+import { FooterSocialComponent } from "../../../../components/footer-social/footer-social.component";
 
 @Component({
   selector: 'app-esporte',
   standalone: true,
-  imports: [HeadingEsporteComponent, HeaderSocialComponent],
+  imports: [HeadingEsporteComponent, HeaderSocialComponent, FooterSocialComponent],
   templateUrl: './esporte.component.html',
   styleUrl: './esporte.component.scss'
 })
