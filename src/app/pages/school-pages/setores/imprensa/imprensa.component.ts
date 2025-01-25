3faff7c54f4b5d7725de6bb10b4f5271a66a62c3
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2025-01-25 16:21:45 -0300

    Angular Animations habilitado - teste com Imprensa

--- src/app/pages/school-pages/setores/imprensa/imprensa.component.ts
+++ src/app/pages/school-pages/setores/imprensa/imprensa.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { FooterSocialComponent } from "../../../../components/footer-social/footer-social.component";
 import { HeaderSocialComponent } from "../../../../components/header-social/header-social.component";
 import { HeadingImprensaComponent } from "./heading-imprensa/heading-imprensa.component";
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-imprensa',
   standalone: true,
   imports: [FooterSocialComponent, HeaderSocialComponent, HeadingImprensaComponent],
   templateUrl: './imprensa.component.html',
-  styleUrl: './imprensa.component.scss'
+  styleUrl: './imprensa.component.scss',
+  animations: [
+    trigger('fadeSlideLeft', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-50px)' }),
+        animate('600ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+      ]),
+    ]),
+    trigger('fadeSlideRight', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(50px)' }),
+        animate('600ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+      ]),
+    ]),
+  ],
 })
+
 export class ImprensaComponent {
 
 }
